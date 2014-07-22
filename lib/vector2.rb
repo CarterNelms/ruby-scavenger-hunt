4b617880ff38a7895818064bb5991951902f0572
CarterNelms <carternelms@bellsouth.net>
2014-07-22 13:39:46 -0500

    wrote test for Vector2 + number

--- lib/vector2.rb
+++ lib/vector2.rb
@@ -5,6 +5,17 @@ class Vector2 < Vector
     Math.sqrt((self[0]-v[0])**2 + (self[1]-v[1])**2)
   end
 
+  # def +(num)
+  #   if(num.class == Float || num.class == Fixnum)
+  #     (0..1).each{|i|
+  #       self[i] += num
+  #     }
+  #   else
+  #     puts num.class
+  #     raise TypeError
+  #   end
+  # end
+
   def self.pathlength(*points)
     length = 0
     points.reduce{|prev_p, next_p|
--- test/vector2_test.rb
+++ test/vector2_test.rb
@@ -1,11 +1,32 @@
-require_relative "../lib/vector2"
+require 'test_helper'
+require_relative '../lib/vector2'
 
-RSpec.describe "Vector2" do
-  context ".distance" do
-    it "should return the correct distance between two 2-dimensional vectors (vector2)" do
-      a = Vector2.new(2, 2)
-      b = Vector2.new(3, 2)
-      a.distance(b).should == 1
-    end
+class Vector2Test < MiniTest::Unit::TestCase
+
+  def test_distance_should_return_correct_distance_between_2_vector2s
+    a = Vector2[2, 2]
+    b = Vector2[3, 2]
+    assert_equal 1, a.distance(b)
   end
+
+  def test_adding_integer_to_vector2_adds_it_to_both_the_x_and_y_elements
+    a = Vector2[2, 2]
+    a = a + 3
+    assert_equal Vector2[5, 5], a
+  end
+
 end
+
+
+
+# require_relative "../lib/vector2"
+
+# RSpec.describe "Vector2" do
+#   context ".distance" do
+#     it "should return the correct distance between two 2-dimensional vectors (vector2)" do
+#       a = Vector2.new(2, 2)
+#       b = Vector2.new(3, 2)
+#       a.distance(b).should == 1
+#     end
+#   end
+# end
