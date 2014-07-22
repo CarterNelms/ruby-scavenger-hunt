2d5ba80767e70868fb7d4e9ba5eb981e323ce4c1
CarterNelms <carternelms@bellsouth.net>
2014-07-22 11:59:19 -0500

    added pathlength

--- lib/vector2.rb
+++ lib/vector2.rb
@@ -1,29 +1,16 @@
 require "matrix"
 
-class Vector2 < Vector::ZeroVectorError
-
-  def initialize(x, y)
-    @x = x
-    @y = y
+class Vector2 < Vector
+  def distance(v)
+    Math.sqrt((self[0]-v[0])**2 + (self[1]-v[1])**2)
   end
 
-  def pos
-    [@x, @y]
-  end
-
-  def distance(other_vector2)
-    Vector2.distance(self.pos, other_vector2.pos)
-  end
-
-  def self.distance(vector2_1, vector2_2)
-    x1 = vector2_1.x
-    y1 = vector2_1.y
-    x2 = vector2_2.x
-    y2 = vector2_2.y
-
-    delta_x = x2-x1
-    delta_y = y2-y1
-
-    Math.sqrt(delta_x**2 + delta_y**2)
+  def self.pathlength(*points)
+    length = 0
+    points.reduce{|prev_p, next_p|
+      length += prev_p.distance(next_p) if prev_p && next_p
+      next_p
+    }
+    length
   end
 end
--- test/vector2_test.rb
+++ test/vector2_test.rb
@@ -1,14 +1,5 @@
 require_relative "../lib/vector2"
 
-# class AboutVector2 < Neo::Koan
-#   def test_should_return_correct_distance_between_two_points
-#       a = Vector2.new(2, 2)
-#       b = Vector2.new(3, 2)
-#       a.distance(b).should == 1
-#   end
-# end
-
-
 RSpec.describe "Vector2" do
   context ".distance" do
     it "should return the correct distance between two 2-dimensional vectors (vector2)" do
